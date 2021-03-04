857f880d94e90b237a60d9613ee134f00f88c792
Peter Harrison <peter.harrison@helicron.net>
2021-03-04 14:50:47 +0000

    move number converters to separate file

--- settings-test/system-commands.cpp
+++ settings-test/system-commands.cpp
@@ -4,7 +4,7 @@
  * File Created: Tuesday, 2nd March 2021 12:33:55 pm                                     *
  * Author: Peter Harrison                                                                *
  * -----                                                                                 *
- * Last Modified: Thursday, 4th March 2021 2:25:14 pm                                    *
+ * Last Modified: Thursday, 4th March 2021 2:48:31 pm                                    *
  * Modified By: Peter Harrison                                                           *
  * -----                                                                                 *
  * Copyright 2017 - 2021 Peter Harrison, Micromouseonline                                *
@@ -33,113 +33,10 @@
  */
 
 #include "system-commands.h"
+#include "read-number.h"
 #include "settings.h"
 #include "stopwatch.h"
 #include <Arduino.h>
-
-enum {
-    T_OK,
-    T_OUT_OF_RANGE,
-    T_UNEXPECTED_TOKEN,
-};
-
-/////////////////////////////////////////////////////////////////////////////
-
-// floats have no more than 8 significant digits
-#define MAX_DIGITS 8
-/***
- * Scan a character array for an integer.
- * Begin scn at line[pos]
- * Assumes no leading spaces.
- * Stops at first non-digit.
- * MODIFIES pos so that it points to the first non-digit
- * MODIFIES value ONLY IF a valid integer is converted
- * RETURNS  boolean status indicating success or error
- *
- * optimisations are possible but may not be worth the effort
- */
-uint8_t read_integer(const char *line, uint8_t *pos, int *value) {
-    char *ptr = (char *)line + *pos;
-    char c = *ptr++;
-    bool is_minus = false;
-    uint8_t digits = 0;
-    if (c == '-') {
-        is_minus = true;
-        c = *ptr++;
-    }
-    int32_t number = 0;
-    while (c >= '0' and c <= '9') {
-        if (digits++ < MAX_DIGITS) {
-            number = 10 * number + (c - '0');
-        }
-        c = *ptr++;
-    }
-    *pos = ptr - line - 1;
-    if (digits > 0) {
-
-        *value = is_minus ? -number : number;
-    }
-    return digits;
-}
-/////////////////////////////////////////////////////////////////////////////
-
-/***
- * Scan a character array for a float.
- * This is a much simplified and limited version of the library function atof()
- * It will not convert exponents and has a limited range of valid values.
- * They should be more than adequate for the robot parameters however.
- * Begin scan at line[pos]
- * Assumes no leading spaces.
- * Only scans MAX_DIGITS characters
- * Stops at first non-digit, or decimal point.
- * MODIFIES pos so that it points to the first character after the number
- * MODIFIES value ONLY IF a valid float is converted
- * RETURNS  boolean status indicating success or error
- *
- * optimisations are possible but may not be worth the effort
- */
-uint8_t read_float(const char *line, uint8_t *pos, float *value) {
-
-    char *ptr = (char *)line + *pos;
-    char c = *ptr++;
-    uint8_t digits = 0;
-
-    bool is_minus = false;
-    if (c == '-') {
-        is_minus = true;
-        c = *ptr++;
-    }
-
-    uint32_t a = 0.0;
-    int exponent = 0;
-    while (c >= '0' and c <= '9') {
-        if (digits++ < MAX_DIGITS) {
-            a = a * 10 + (c - '0');
-        }
-        c = *ptr++;
-    };
-    if (c == '.') {
-        c = *ptr++;
-        while (c >= '0' and c <= '9') {
-            if (digits++ < MAX_DIGITS) {
-                a = a * 10 + (c - '0');
-                exponent = exponent - 1;
-            }
-            c = *ptr++;
-        }
-    }
-
-    *pos = ptr - line - 1;
-    float b = a;
-    while (exponent < 0) {
-        b *= 0.1;
-        exponent++;
-    }
-    if (digits > 0) {
-        *value = is_minus ? -b : b;
-    }
-    return digits;
-}
 
 int execute_settings_command(char *line) {
     // we already know that line[0] == '$'
